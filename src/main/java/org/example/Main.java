56cdba12fa485ba1f15ec9458ca6a48d7f40d9a6
Buba <105680119+Buber11@users.noreply.github.com>
2023-04-19 23:11:42 +0200

    add test class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //dadada
+        //dadada..
         File file = new File("słowa.txt");
         ArrayList<String> words = new ArrayList<>();
         Scanner scanner;
